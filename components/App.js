5d4ea74090c205c7c34b15bfe94f05a77638304c
Enyinna <enyinna.tech@gmail.com>
2020-06-13 21:38:37 +0100

    Initialized application skeleton

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>Hello</div>
+  );
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
