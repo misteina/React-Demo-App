39d2d642b0d53d98a3567dd5cb83e86d4dd5b4bb
Enyinna <enyinna.tech@gmail.com>
2020-06-14 10:51:39 +0100

    Update application directory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (
-    <div>Hello</div>
-  );
-}
+import App from './components/App';
 
 ReactDOM.render(
     <React.StrictMode>
